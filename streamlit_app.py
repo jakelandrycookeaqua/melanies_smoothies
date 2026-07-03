133739dd2f705b5e16d92944b0c25719b3a115ee
jakelandrycookeaqua <jake.landry@cookeaqua.com>
2026-07-03 14:50:00 -0300

    Update streamlit_app.py
    editted multi select to show nutritional information based on fruits selected.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!")
@@ -14,7 +15,8 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -27,10 +29,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
-
-    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
@@ -40,3 +42,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
     
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+      
+
